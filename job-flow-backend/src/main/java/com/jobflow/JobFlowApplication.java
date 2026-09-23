b11813c0622254bd250c77df3630467e048834ac
jianweicheng0822 <jianweicheng0822@gmail.com>
2026-09-22 21:58:25 -0600

    Enable scheduling for interview reminder tasks

--- job-flow-backend/src/main/java/com/jobflow/JobFlowApplication.java
+++ job-flow-backend/src/main/java/com/jobflow/JobFlowApplication.java
@@ -2,8 +2,10 @@ package com.jobflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JobFlowApplication {
 
     public static void main(String[] args) {
